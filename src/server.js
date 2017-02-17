ef0bf6c78b34087da4b493c7535bf033db12cf04
Markus Felten <Markus.Felten@gmx.de>
2017-02-17 18:08:53 +0100

    feat: specify sqlite file over command line args

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('sample.sqlite3');
+/* jslint node: true, esnext: true */
 
-db.serialize(function () {
+'use struct';
+
+const program = require('commander');
+const sqlite3 = require('sqlite3').verbose();
+
+program
+  .description('Konsum server')
+//  .version(module.exports.version)
+  .option('-db, --database <file>', 'database file')
+  .parse(process.argv);
+
+const db = new sqlite3.Database(program.database);
+
+db.serialize( () => {
   db.run("CREATE TABLE lorem (info TEXT)");
 
   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
+  for (let i = 0; i < 10; i++) {
     stmt.run("Ipsum " + i);
   }
   stmt.finalize();
 
-  db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
-    console.log(row.id + ": " + row.info);
-  });
+  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => console.log(row.id + ": " + row.info));
 });
 
 db.close();
