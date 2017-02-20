ce754b9c4d0d5df47bf1a2b33e9753de210635f7
Markus Felten <Markus.Felten@gmx.de>
2017-02-20 23:20:28 +0100

    feat: init database from config

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@
 'use struct';
 
 const path = require('path'),
-program = require('commander'),
-sqlite3 = require('sqlite3').verbose();
+  program = require('commander'),
+  sqlite3 = require('sqlite3').verbose();
 import {
   expand
 }
@@ -12,30 +12,43 @@ from 'config-expander';
 
 program
   .description('Konsum server')
-//  .version(module.exports.version)
+  //  .version(module.exports.version)
   .option('-c, --config <file>', 'use config from file')
   .parse(process.argv);
 
-  const constants = {
-    basedir: path.dirname(program.config || process.cwd()),
-    installdir: path.resolve(__dirname, '..')
-  };
+const constants = {
+  basedir: path.dirname(program.config || process.cwd()),
+  installdir: path.resolve(__dirname, '..')
+};
 
-expand(program.config ? "${include('" + path.basename(program.config) + "')}" : { database: 'sample.sqlite'}, { constants })
-.then(config => {
-  const db = new sqlite3.Database(config.database);
-
-  db.serialize( () => {
-    db.run("CREATE TABLE lorem (info TEXT)");
-
-    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
-
-    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => console.log(row.id + ": " + row.info));
+expand(program.config ? "${include('" + path.basename(program.config) + "')}" : {
+    database: 'sample.sqlite'
+  }, {
+    constants
+  })
+  .then(config => {
+    prepareDatabase(config.database).then(db => {
+      db.close();
+    });
   });
 
-  db.close();
-});
+
+function prepareDatabase(file) {
+  const db = new sqlite3.Database(config.database);
+
+  return new Promise((fullull, reject) => {
+    db.serialize(() => {
+      db.run("CREATE TABLE lorem (info TEXT)");
+
+      var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+      for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
+
+      db.each("SELECT rowid AS id, info FROM lorem", (err, row) => console.log(row.id + ": " + row.info));
+
+      fullfill(db);
+    });
+  });
+}
