9133e9d0883db181337348f80f412ca8483f8751
Markus Felten <Markus.Felten@gmx.de>
2017-02-24 22:24:19 +0100

    refactor: separate http stuff into its own module

---
+++ src/http.js
@@ -0,0 +1,50 @@
+/* jslint node: true, esnext: true */
+
+'use struct';
+
+const http = require('http'),
+https = require('https'),
+querystring = require('querystring'),
+jsonwebtoken = require('jsonwebtoken'),
+Koa = require('koa'),
+KoaJWT = require('koa-jwt'),
+Router = require('koa-better-router');
+
+
+export function prepareHttpServer(config) {
+  const app = new Koa();
+// if there is a cert configured use https otherwise plain http
+const server = config.http.cert ? https.createServer(config.http, app.callback()) : http.createServer(app.callback());
+server.on('error', err => console.log(err));
+
+const router = Router();
+router.addRoute('GET', '/login', (ctx, next) => new Promise((fullfill, reject) => {
+  const q = querystring.parse(ctx.request.querystring);
+
+  if (q.user === 'admin' && q.password === 'start123') {
+    const claims = {
+      permissions: 'all',
+      iss: 'http://myDomain'
+    };
+    const token = jsonwebtoken.sign(claims, config.http.auth.jwt.private, {
+      algorithm: 'RS256',
+      expiresIn: config.http.auth.jwt.expires || '12h'
+    });
+    ctx.status = 200;
+    ctx.body = {
+      token, message: 'Successfully logged in!'
+    };
+  } else {
+    ctx.status = 401;
+    ctx.body = {
+      message: 'Authentication failed'
+    };
+  }
+  fullfill(next());
+}));
+
+app.use(router.middleware());
+app.listen(config.http.port, () => console.log(`Listening on port ${config.http.port}`));
+
+return Promise.resolve({ app, server});
+}
--- src/server.js
+++ src/server.js
@@ -4,14 +4,7 @@
 
 const path = require('path'),
   program = require('commander'),
-  http = require('http'),
-  https = require('https'),
-  sqlite3 = require('sqlite3').verbose(),
-  querystring = require('querystring'),
-  jsonwebtoken = require('jsonwebtoken'),
-  Koa = require('koa'),
-  KoaJWT = require('koa-jwt'),
-  Router = require('koa-better-router');
+  sqlite3 = require('sqlite3').verbose();
 
 import {
   expand
@@ -22,6 +15,11 @@ import {
   prepareDatabase
 }
 from './database';
+
+import {
+  prepareHttpServer
+}
+from './http';
 
 program
   .description('Konsum server')
@@ -44,42 +42,10 @@ const defaultConfig = {
 expand(program.config ? "${include('" + path.basename(program.config) + "')}" : defaultConfig, {
     constants
   })
-  .then(config => prepareDatabase(config).then(db => {
-
-    const app = new Koa();
-
-    // if there is a cert configured use https otherwise plain http
-    const server = config.http.cert ? https.createServer(config.http, app.callback()) : http.createServer(app.callback());
-    server.on('error', err => console.log(err));
-
-    const router = Router();
-    router.addRoute('GET', '/login', (ctx, next) => new Promise((fullfill, reject) => {
-      const q = querystring.parse(ctx.request.querystring);
-
-      if (q.user === 'admin' && q.password === 'start123') {
-        const claims = {
-          permissions: 'all',
-          iss: 'http://myDomain'
-        };
-        const token = jsonwebtoken.sign(claims, config.http.auth.jwt.private, {
-          algorithm: 'RS256',
-          expiresIn: config.http.auth.jwt.expires || '12h'
-        });
-        ctx.status = 200;
-        ctx.body = {
-          token, message: 'Successfully logged in!'
-        };
-      } else {
-        ctx.status = 401;
-        ctx.body = {
-          message: 'Authentication failed'
-        };
-      }
-      fullfill(next());
-    }));
-
-    app.use(router.middleware());
-    app.listen(config.http.port, () => console.log(`Listening on port ${config.http.port}`));
+  .then(config => Promise.all([
+    prepareDatabase(config),
+    prepareHttpServer(config)])
+  .then(([db,http]) => {
 
     db.close();
   }));
