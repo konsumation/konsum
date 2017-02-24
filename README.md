fae93ec73f30a6523dfe6ef35a0341f53e6d79b0
Markus Felten <Markus.Felten@gmx.de>
2017-02-24 18:49:52 +0100

    feat: /login route generates a jwt token

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ create self signed cert
 ```shell
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
 ```
+
+token setup
+===========
+
+```shell
+openssl genrsa -out demo.rsa 1024
+openssl rsa -in demo.rsa -pubout > demo.rsa.pub
+```
--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ const path = require('path'),
   http = require('http'),
   https = require('https'),
   sqlite3 = require('sqlite3').verbose(),
-  Koa = require('koa');
+  querystring = require('querystring'),
+  jsonwebtoken = require('jsonwebtoken'),
+  Koa = require('koa'),
+  KoaJWT = require('koa-jwt'),
+  Router = require('koa-better-router');
 
 import {
   expand
@@ -37,7 +41,9 @@ const defaultConfig = {
   }
 };
 
-expand(program.config ? "${include('" + path.basename(program.config) + "')}" : defaultConfig, { constants })
+expand(program.config ? "${include('" + path.basename(program.config) + "')}" : defaultConfig, {
+    constants
+  })
   .then(config => prepareDatabase(config).then(db => {
 
     const app = new Koa();
@@ -46,6 +52,33 @@ expand(program.config ? "${include('" + path.basename(program.config) + "')}" : 
     const server = config.http.cert ? https.createServer(config.http, app.callback()) : http.createServer(app.callback());
     server.on('error', err => console.log(err));
 
+    const router = Router();
+    router.addRoute('GET', '/login', (ctx, next) => new Promise((fullfill, reject) => {
+      const q = querystring.parse(ctx.request.querystring);
+
+      if (q.user === 'admin' && q.password === 'start123') {
+        const claims = {
+          permissions: 'all',
+          iss: 'http://myDomain'
+        };
+        const token = jsonwebtoken.sign(claims, config.http.auth.jwt.private, {
+          algorithm: 'RS256',
+          expiresIn: config.http.auth.jwt.expires || '12h'
+        });
+        ctx.status = 200;
+        ctx.body = {
+          token, message: 'Successfully logged in!'
+        };
+      } else {
+        ctx.status = 401;
+        ctx.body = {
+          message: 'Authentication failed'
+        };
+      }
+      fullfill(next());
+    }));
+
+    app.use(router.middleware());
     app.listen(config.http.port, () => console.log(`Listening on port ${config.http.port}`));
 
     db.close();
