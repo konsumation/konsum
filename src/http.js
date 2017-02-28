cad5949338a2d19013402517eab0dabc5a60b99b
Markus Felten <Markus.Felten@gmx.de>
2017-02-28 16:30:45 +0100

    feat: try koajwt routes

--- src/http.js
+++ src/http.js
@@ -3,57 +3,66 @@
 'use struct';
 
 const http = require('http'),
-https = require('https'),
-querystring = require('querystring'),
-jsonwebtoken = require('jsonwebtoken'),
-Koa = require('koa'),
-KoaJWT = require('koa-jwt'),
-Router = require('koa-better-router');
+  https = require('https'),
+  querystring = require('querystring'),
+  jsonwebtoken = require('jsonwebtoken'),
+  Koa = require('koa'),
+  KoaJWT = require('koa-jwt'),
+  Router = require('koa-better-router');
 
 
 export function prepareHttpServer(config) {
   const app = new Koa();
-// if there is a cert configured use https otherwise plain http
-const server = config.http.cert ? https.createServer(config.http, app.callback()) : http.createServer(app.callback());
-server.on('error', err => console.log(err));
+  // if there is a cert configured use https, otherwise plain http
+  const server = config.http.cert ? https.createServer(config.http, app.callback()) : http.createServer(app.callback());
+  server.on('error', err => console.log(err));
 
-const router = Router();
-router.addRoute('GET', '/login', (ctx, next) => new Promise((fullfill, reject) => {
-  const q = querystring.parse(ctx.request.querystring);
+  const router = Router();
+  router.addRoute('GET', '/login', (ctx, next) => new Promise((fullfill, reject) => {
+    const q = querystring.parse(ctx.request.querystring);
 
-  if (q.user === 'admin' && q.password === 'start123') {
-    const claims = {
-      permissions: 'all',
-      iss: 'http://myDomain'
-    };
-    const token = jsonwebtoken.sign(claims, config.http.auth.jwt.private, {
-      algorithm: 'RS256',
-      expiresIn: config.http.auth.jwt.expires || '12h'
-    });
-    ctx.status = 200;
-    ctx.body = {
-      token, message: 'Successfully logged in!'
-    };
-  } else {
-    ctx.status = 401;
-    ctx.body = {
-      message: 'Authentication failed'
-    };
-  }
-  fullfill(next());
-}));
+    if (q.user === 'admin' && q.password === 'start123') {
+      const claims = {
+        permissions: 'all',
+        iss: 'http://myDomain'
+      };
+      const token = jsonwebtoken.sign(claims, config.http.auth.jwt.private, {
+        algorithm: 'RS256',
+        expiresIn: config.http.auth.jwt.expires || '12h'
+      });
+      ctx.status = 200;
+      ctx.body = {
+        token, message: 'Successfully logged in!'
+      };
+    } else {
+      ctx.status = 401;
+      ctx.body = {
+        message: 'Authentication failed'
+      };
+    }
+    fullfill(next());
+  }));
 
-router.addRoute('GET', '/values', (ctx, next) => {
-  ctx.body = [{
-    date: new Date(),
-    value: 1.0
-  }];
+  app.use(router.middleware());
 
-  return next();
-});
+  /*
+  app.use(KoaJWT({
+    secret: config.http.auth.jwt.public
+  }));
+  */
 
-app.use(router.middleware());
-app.listen(config.http.port, () => console.log(`Listening on port ${config.http.port}`));
+  router.addRoute('GET', '/values', (ctx, next) => {
+    ctx.body = [{
+      date: new Date(),
+      value: 1.0
+    }];
 
-return Promise.resolve({ app, server, router });
+    return next();
+  });
+
+  app.listen(config.http.port, () => console.log(`Listening on port ${config.http.port}`));
+
+  return Promise.resolve({
+    app, server, router
+  });
 }
