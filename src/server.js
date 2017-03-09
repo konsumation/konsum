624337981439541d011ff047ee3ee159c84e47e8
Markus Felten <Markus.Felten@gmx.de>
2017-03-09 17:41:42 +0100

    docs: added some comments

--- src/server.js
+++ src/server.js
@@ -23,32 +23,37 @@ from './http';
 
 program
   .description('Konsum server')
-  .version(require(path.join(__dirname,'..','package.json')).version)
+  .version(require(path.join(__dirname, '..', 'package.json')).version)
   .option('-c, --config <file>', 'use config from file')
-  .action(async (args, options, logger) => {
+  .action(async(args, options, logger) => {
+
+    // some constants to be used while loading the configuration
     const constants = {
-      basedir: path.dirname(options.config || process.cwd()),
-      installdir: path.resolve(__dirname, '..')
+      basedir: path.dirname(options.config || process.cwd()), // where is the config file located
+      installdir: path.resolve(__dirname, '..') // make references to the installdir possible
     };
 
+    // default config if none is given
     const defaultConfig = {
       database: {
-        file : 'sample.sqlite'
+        file: 'sample.sqlite'
       },
       http: {
         port: 123456
       }
     };
 
-    const config = await expand(options.config
-          ? "${include('" + path.basename(options.config) + "')}"
-          : defaultConfig,
-          { constants });
+    // load config and expand expressions ${something} inside
+    const config = await expand(options.config ? "${include('" + path.basename(options.config) + "')}" :
+      defaultConfig, {
+        constants
+      });
 
+    // prepare the database with the config
     const db = await prepareDatabase(config);
-    const http = await prepareHttpServer(config, db);
 
-    db.close();
+    // prepare the web-server with the config and the database
+    const http = await prepareHttpServer(config, db);
   });
 
 program
