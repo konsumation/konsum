3cac633031ce55f56c0296640df2d7b32fdc38f6
Markus Felten <Markus.Felten@gmx.de>
2017-11-10 23:55:34 +0100

    chore(scripts): remove prepublish@rollup --output=bin/konsum -c -- src/server.js && chmod +x bin/konsum chore(scripts): update cover@nyc --temp-directory build/nyc ava from template chore(scripts): update precover@rollup -c tests/rollup.config.js from template chore(scripts): update test@ava from template chore(scripts): update posttest@npm run prepare && markdown-doctest from template chore(scripts): add prepare@rollup -c from template chore(scripts): add docs@jsdoc2md --configure doc/jsdoc.json -l off -t doc/README.hbs -f src/*.js >README.md from template chore(scripts): update semantic-release@semantic-release from template chore(devDependencies): remove istanbul@^0.4.5 chore(devDependencies): remove mocha@^4.0.1 chore(devDependencies): remove chai@^4.0.2 chore(devDependencies): update jsdoc-to-markdown@^3.0.1 from template chore(devDependencies): add jsdoc-babel@^0.3.0 from template chore(devDependencies): add nyc@^11.3.0 from template chore(devDependencies): add ava@^0.23.0 from template chore(devDependencies): update rollup@^0.51.3 from template chore(devDependencies): update semantic-release@^9.1.1 from template chore(devDependencies): add xo@^0.19.0 from template chore(engines): update node@>=8.9.1 from template chore(package): set template repo docs(package): remove keyword null chore(travis): remove node version(s) 7.6 docs(README): update from template chore(rollup): update from template chore(rollup): update from template chore(git): update .gitignore from template chore(npm): update .npmignore from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,15 @@
+import pkg from './package.json';
+
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  banner: '#!/usr/bin/env node',
-  format: 'cjs',
   plugins: [nodeResolve(), commonjs()],
-  external: ['config-expander']
+  external: ['config-expander'],
+  input: pkg.module,
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+    banner: '#!/usr/bin/env node'
+  }
 };
