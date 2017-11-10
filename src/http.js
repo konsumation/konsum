ebb590c51839f58d333a23e9b9bf647538c4bb7a
Markus Felten <Markus.Felten@gmx.de>
2017-11-10 23:54:51 +0100

    style: prettier

--- src/http.js
+++ src/http.js
@@ -1,7 +1,3 @@
-/* jslint node: true, esnext: true */
-
-'use struct';
-
 const http = require('http'),
   https = require('https'),
   querystring = require('querystring'),
@@ -10,11 +6,12 @@ const http = require('http'),
   KoaJWT = require('koa-jwt'),
   Router = require('koa-better-router');
 
-
 export function prepareHttpServer(config, database) {
   const app = new Koa();
   // if there is a cert configured use https, otherwise plain http
-  const server = config.http.cert ? https.createServer(config.http, app.callback()) : http.createServer(app.callback());
+  const server = config.http.cert
+    ? https.createServer(config.http, app.callback())
+    : http.createServer(app.callback());
   server.on('error', err => console.log(err));
 
   const router = Router();
@@ -31,15 +28,21 @@ export function prepareHttpServer(config, database) {
         permissions: user.roles.join(','),
         iss: 'http://myDomain'
       };
-      const token = jsonwebtoken.sign(claims, config.http.auth.jwt.private,
-        mergeDefaults({
-          algorithm: 'RS256',
-          expiresIn: '12h'
-        }, config.http.auth.jwt)
+      const token = jsonwebtoken.sign(
+        claims,
+        config.http.auth.jwt.private,
+        mergeDefaults(
+          {
+            algorithm: 'RS256',
+            expiresIn: '12h'
+          },
+          config.http.auth.jwt
+        )
       );
       ctx.status = 200;
       ctx.body = {
-        token, message: 'Successfully logged in!'
+        token,
+        message: 'Successfully logged in!'
       };
     } else {
       ctx.status = 401;
@@ -57,25 +60,33 @@ export function prepareHttpServer(config, database) {
     secret: config.http.auth.jwt.public
   });
 
-  router.addRoute('GET', '/values', /*restricted,*/ (ctx, next) =>
-    new Promise((fullfill, reject) =>
-      database.all('SELECT date, type, amount FROM Konsum', (err, rows) => {
-        if (err) {
-          ctx.status = 401;
-          ctx.body = err;
-          reject(err);
-        } else {
-          ctx.body = rows;
-          fullfill(next());
-        }
-      })
-    )
+  router.addRoute(
+    'GET',
+    '/values',
+    /*restricted,*/ (ctx, next) =>
+      new Promise((fullfill, reject) =>
+        database.all('SELECT date, type, amount FROM Konsum', (err, rows) => {
+          if (err) {
+            ctx.status = 401;
+            ctx.body = err;
+            reject(err);
+          } else {
+            ctx.body = rows;
+            fullfill(next());
+          }
+        })
+      )
   );
 
-  app.listen(config.http.port, () => console.log(`Listening on port ${config.http.port}`));
+  app.listen(config.http.port, () =>
+    console.log(`Listening on port ${config.http.port}`)
+  );
 
   return Promise.resolve({
-    app, server, router, restricted
+    app,
+    server,
+    router,
+    restricted
   });
 }
 
@@ -90,7 +101,7 @@ function mergeDefaults(a1, a2) {
   const t = {};
 
   if (a2) {
-    Object.keys(a1).forEach(k => t[k] = a2[k]);
+    Object.keys(a1).forEach(k => (t[k] = a2[k]));
   }
 
   return Object.assign({}, a1, t);
--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,19 +1,21 @@
-/* jslint node: true, esnext: true */
-'use strict';
-
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import istanbul from 'rollup-plugin-istanbul';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  entry: 'tests/*_test.js',
+  entry: 'tests/*-test.js',
   format: 'cjs',
   dest: 'build/bundle.test.js',
-  plugins: [istanbul({
-    exclude: ['tests/*.js', 'node_modules/**/*']
-  }), nodeResolve({
-    jsnext: true
-  }), commonjs(), multiEntry()],
+  plugins: [
+    istanbul({
+      exclude: ['tests/*.js', 'node_modules/**/*']
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs(),
+    multiEntry()
+  ],
   external: ['buffer']
 };
