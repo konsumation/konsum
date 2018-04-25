1a9763d90b665340f60e62d1dcbc3130f98434db
Markus Felten <Markus.Felten@gmx.de>
2018-04-25 22:41:40 +0200

    chore: use rollup-plugin-executable for bin/*

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
 import pkg from './package.json';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import executable from 'rollup-plugin-executable';
 
-export default {
-  input: pkg.module,
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    banner: '#!/usr/bin/env node'
-  },
-  external: ['config-expander'],
-  plugins: [nodeResolve(), commonjs()]
-};
+export default [
+  ...Object.keys(pkg.bin).map(name => {
+    return {
+      input: `src/${name}.js`,
+      output: {
+        file: pkg.bin[name],
+        format: 'cjs',
+        banner: '#!/usr/bin/env node'
+      },
+      external: ['config-expander'],
+      plugins: [nodeResolve(), commonjs(), executable()]
+    };
+  }),
+  {
+    input: pkg.module,
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      banner: '#!/usr/bin/env node'
+    },
+    external: ['config-expander'],
+    plugins: [nodeResolve(), commonjs()]
+  }
+];
