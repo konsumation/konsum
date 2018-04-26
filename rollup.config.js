a321391a8c448167ef6979cbcd8fa79a0137d404
Markus Felten <Markus.Felten@gmx.de>
2018-04-26 07:16:06 +0200

    fix(rollup): no banner for pkg.main

--- rollup.config.js
+++ rollup.config.js
@@ -20,8 +20,7 @@ export default [
     input: pkg.module,
     output: {
       file: pkg.main,
-      format: 'cjs',
-      banner: '#!/usr/bin/env node'
+      format: 'cjs'
     },
     external: ['config-expander'],
     plugins: [nodeResolve(), commonjs()]
