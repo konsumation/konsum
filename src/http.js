ee1681821755cb7e950ded2d4be5b87785773ab0
k0nsti <kkarpman@clickforit.de>
2018-12-10 18:28:53 +0100

    fix(values): the route returns all data from database as an object

--- src/http.js
+++ src/http.js
@@ -1,10 +1,10 @@
-const http = require('http'),
-  https = require('https'),
-  querystring = require('querystring'),
-  jsonwebtoken = require('jsonwebtoken'),
-  Koa = require('koa'),
-  KoaJWT = require('koa-jwt'),
-  Router = require('koa-better-router');
+const http = require("http"),
+  https = require("https"),
+  querystring = require("querystring"),
+  jsonwebtoken = require("jsonwebtoken"),
+  Koa = require("koa"),
+  KoaJWT = require("koa-jwt"),
+  Router = require("koa-better-router");
 
 export function prepareHttpServer(config, database) {
   const app = new Koa();
@@ -12,28 +12,28 @@ export function prepareHttpServer(config, database) {
   const server = config.http.cert
     ? https.createServer(config.http, app.callback())
     : http.createServer(app.callback());
-  server.on('error', err => console.log(err));
+  server.on("error", err => console.log(err));
   const router = Router();
 
   /**
    * login to request api token
    */
-  router.addRoute('GET', '/login', (ctx, next) => {
+  router.addRoute("GET", "/login", (ctx, next) => {
     const q = querystring.parse(ctx.request.querystring);
     const user = config.users[q.user];
 
     if (user !== undefined && user.password === q.password) {
       const claims = {
-        permissions: user.roles.join(','),
-        iss: 'http://myDomain'
+        permissions: user.roles.join(","),
+        iss: "http://myDomain"
       };
       const token = jsonwebtoken.sign(
         claims,
         config.http.auth.jwt.private,
         mergeDefaults(
           {
-            algorithm: 'RS256',
-            expiresIn: '12h'
+            algorithm: "RS256",
+            expiresIn: "12h"
           },
           config.http.auth.jwt
         )
@@ -41,12 +41,12 @@ export function prepareHttpServer(config, database) {
       ctx.status = 200;
       ctx.body = {
         token,
-        message: 'Successfully logged in!'
+        message: "Successfully logged in!"
       };
     } else {
       ctx.status = 401;
       ctx.body = {
-        message: 'Authentication failed'
+        message: "Authentication failed"
       };
     }
     return next();
@@ -60,24 +60,27 @@ export function prepareHttpServer(config, database) {
   });
 
   router.addRoute(
-    'GET',
-    '/values',
-      /*restricted,*/(ctx, next) =>
+    "GET",
+    "/values",
+    /*restricted,*/ (ctx, next) =>
       new Promise((fullfill, reject) =>
-        database.createReadStream()
-          .on('data', function (data) {
-            console.log(data.key, '=', data.value);
+        database
+          .createReadStream()
+          .on("data", function(data) {
+            console.log(data.key, "=", data.value);
+            console.log('data.key, "=", data.value');
+            ctx.body = Object.assign(ctx.body,data);
           })
-          .on('error', function (err) {
+          .on("error", function(err) {
             ctx.status = 401;
             ctx.body = err;
             reject(err);
           })
-          .on('close', function () {
-            console.log('Stream closed')
+          .on("close", function() {
+            console.log("Stream closed");
           })
-          .on('end', function () {
-            ctx.body = data;
+          .on("end", function() {
+            console.log('Stream ended');
             fullfill(next());
           })
       )
@@ -106,7 +109,6 @@ function mergeDefaults(a1, a2) {
   const t = {};
 
   Object.keys(a1).forEach(k => (t[k] = a2[k] ? a2[k] : a1[k]));
-
 
   return Object.assign({}, a1, t);
 }
