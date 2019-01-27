14c0daa45100aa2747182d1ee080dc8f3af7cff3
Markus Felten <Markus.Felten@gmx.de>
2019-01-27 13:10:57 +0100

    test: use tmp for db

--- tests/db-test.js
+++ tests/db-test.js
@@ -1,9 +1,9 @@
-import test from 'ava';
-import tmp from 'tmp';
+import test from "ava";
+import tmp from "tmp";
 
-import { prepareDatabase } from '../src/database';
+import { prepareDatabase } from "../src/database";
 
-const level = require('level');
+const level = require("level");
 
 const config = {
   database: {
@@ -11,36 +11,37 @@ const config = {
   }
 };
 
-
-test('insert after create', async t => {
+test("insert after create", async t => {
   const db = prepareDatabase(config);
-  db.put('foo', 'bar');
-  const value = await db.get('foo');
-  t.is(value, 'bar')
-  db.close();
+  db.put("foo", "bar");
+  const value = await db.get("foo");
+  t.is(value, "bar");
+  await db.close();
 });
 
+test.cb("test db", t => {
+  const config = {
+    database: {
+      path: tmp.tmpNameSync()
+    }
+  };
 
+  const db = prepareDatabase(config);
 
-test.cb('test db', t => {
-  const db = level('./mydb')
-  db.put('k1', 'v1');
-  db.put('k2', 'v2');
-  //console.log(await db.get('k1'));~
-   db.createReadStream()
-    .on('data', function (data) {
-      console.log('hallo')
-      console.log(data.key, '=', data.value)
+  db.put("k1", "v1");
+  db.put("k2", "v2");
+  db.createReadStream()
+    .on("data", data => {
+      console.log(data.key, "=", data.value);
     })
-    .on('error', function (err) {
-      console.log('Oh my!', err)
+    .on("error", err => {
+      console.log("Oh my!", err);
     })
-    .on('close', function () {
-      console.log('Stream closed')
+    .on("close", () => {
+      console.log("Stream closed");
     })
-    .on('end', function () {
-      console.log('Stream ended')
+    .on("end", () => {
+      console.log("Stream ended");
       t.end();
-    })
-
+    });
 });
