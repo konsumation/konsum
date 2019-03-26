a23c219b8221d6226f3bc932d01c4c5878d5f770
Markus Felten <Markus.Felten@gmx.de>
2019-03-26 23:42:05 +0100

    feat: log categories & values during startup

--- src/database.mjs
+++ src/database.mjs
@@ -1,13 +1,19 @@
-
 import levelup from "levelup";
 import leveldown from "leveldown";
-import { initialize } from "konsum-db";
+import { initialize, Category } from "konsum-db";
 
 export async function prepareDatabase(config) {
   const db = await levelup(leveldown(config.database.path));
 
   const master = await initialize(db);
   console.log("DATABASE", master);
+
+  for await (const c of Category.entries(db)) {
+    console.log("CATEGORY", c.name);
+    for await (const { time, value } of c.values(db)) {
+      console.log(c.name, time, value);
+    }
+  }
+
   return db;
 }
-
