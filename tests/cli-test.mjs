7d10be613d0898d32bfe992d73fc74da55b1b6b0
Markus Felten <Markus.Felten@db.com>
2019-05-20 08:28:57 +0200

    test: set cwd=tests

--- tests/cli-test.mjs
+++ tests/cli-test.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const here = dirname(fileURLToPath(import.meta.url));
 
 test('cli', async t => {
-  const p = await execa(join(here,'..','bin','konsum'));
+  const p = await execa(join(here,'..','bin','konsum'),[],{cwd:here});
 
   t.truthy(p);
 });
