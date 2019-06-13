549d152688385bfd9c8a80d835aab9e4d710ba8d
Markus Felten <Markus.Felten@gmx.de>
2019-06-13 10:19:05 +0200

    test: write tmp files into build

--- tests/cli-test.mjs
+++ tests/cli-test.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const here = dirname(fileURLToPath(import.meta.url));
 
 test('cli', async t => {
-  const p = await execa(join(here,'..','bin','konsum'),[],{cwd:here});
+  const p = await execa(join(here,'..','bin','konsum'),[],{cwd:join(here,'..','build')});
 
   t.truthy(p);
 });
