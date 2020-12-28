a8239a61a5d7ec1eb55b10d7439a163765d84413
Markus Felten <Markus.Felten@gmx.de>
2020-12-28 22:07:16 +0100

    test: no cwd required

--- tests/cli-test.mjs
+++ tests/cli-test.mjs
@@ -4,8 +4,7 @@ import execa from "execa";
 test("cli version", async t => {
   const p = await execa(
     new URL("../src/konsum-cli.mjs", import.meta.url).pathname,
-    ["--config", new URL("../config", import.meta.url).pathname, "--version"],
-    { cwd: new URL("../build", import.meta.url).pathname }
+    ["--config", new URL("../config", import.meta.url).pathname, "--version"]
   );
 
   t.regex(p.stdout, /\d+/);
