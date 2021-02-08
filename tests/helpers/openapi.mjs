998141ccc246929d024f3611006cfa74465c7c87
Markus Felten <Markus.Felten@gmx.de>
2021-02-08 18:55:32 +0100

    test: wip openapi

--- tests/helpers/openapi.mjs
+++ tests/helpers/openapi.mjs
@@ -6,15 +6,20 @@ export async function loadOpenAPI(t, path) {
 }
 
 async function assertResponse(t, response, erc, er, expected) {
+
+  if(expected && expected.response) {
+    expected = expected.response;
+  }
+
   if (er && er.content) {
     for (const [ct, c] of Object.entries(er.content)) {
       switch (ct) {
         case "application/json":
           const body = JSON.parse(response.body);
           if (Array.isArray(body)) {
-            t.deepEqual(body, expected[erc]);
+            t.deepEqual(body, expected[erc], `${erc}`);
           } else {
-            t.like(body, expected[erc]);
+            t.like(body, expected[erc], `${erc}`);
           }
           break;
 
@@ -39,21 +44,26 @@ async function assertResponse(t, response, erc, er, expected) {
 }
 
 export async function assertOpenapiPath(t, path, expected) {
+
   const p = t.context.api.paths[path];
   t.truthy(p, `Does not exists in api: ${path}`);
 
   const headers = { Authorization: `Bearer ${t.context.token}` };
-  
+
   for (const [emn, em] of Object.entries(p)) {
     async function handleError(e) {
-      const statusCode = e.response.statusCode;
-      await assertResponse(
-        t,
-        e.response,
-        statusCode,
-        em.responses[statusCode],
-        expected.response ? expected.response : expected
-      );
+      if (e.response) {
+        const statusCode = e.response.statusCode;
+        await assertResponse(
+          t,
+          e.response,
+          statusCode,
+          em.responses[statusCode],
+          expected.response ? expected.response : expected
+        );
+      } else {
+        t.log(`No response from ${path}`, e);
+      }
     }
 
     switch (emn) {
@@ -82,13 +92,17 @@ export async function assertOpenapiPath(t, path, expected) {
         }
         break;
       case "post":
-        try {
-          const response = await got.post(`${t.context.url}${path}`, {
-            headers,
-            json: expected.post
-          });
-        } catch (e) {
-          await handleError(e);
+        for (const [erc, er] of Object.entries(em.responses)) {
+          try {
+            const response = await got.post(`${t.context.url}${path}`, {
+              headers,
+              json: expected.post
+            });
+
+            await assertResponse(t, response, erc, er, expected);
+          } catch (e) {
+            await handleError(e);
+          }
         }
         break;
       case "delete":
@@ -114,4 +128,4 @@ export async function openapiPathTest(t, path, expected) {
 }
 
 openapiPathTest.title = (providedTitle = "openapi", path, expected) =>
-  `${providedTitle} ${path}`.trim();
+  `${providedTitle} ${path} ${Object.keys(expected)}`.trim();
