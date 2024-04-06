82ac57e04bf1e23872a63b773cf2ddb0d452f80d
Markus Felten <Markus.Felten@gmx.de>
2024-04-06 14:42:31 +0200

    feat: support several db types (+postgresql)

--- src/database.mjs
+++ src/database.mjs
@@ -1,12 +1,29 @@
-import Master from "@konsumation/db-level";
+async function loadDrivers(names) {
+  const drivers = {};
+
+  for (const name of names) {
+    const driver = await import(name);
+    drivers[driver.default.name] = driver.default;
+  }
+
+  return drivers;
+}
 
 export const defaultDatabaseConfig = {
   database: {
-    file: "${statedir}/db"
+    level: "${statedir}/db"
   }
 };
 
 export async function prepareDatabase(config) {
-  const master = await Master.initialize(config.database.file);
-  return { master };
+  const dbTypes = await loadDrivers([
+    "@konsumation/db-level",
+    "@konsumation/db-postgresql"
+  ]);
+
+  for (const [driverName, option] of Object.entries(config.database)) {
+    return { master: await dbTypes[driverName].initialize(option) };
+  }
+
+  return {};
 }
