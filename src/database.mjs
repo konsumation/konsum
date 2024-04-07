84f3dd6ebcf8a3def75031372bbd5a1497563900
Markus Felten <Markus.Felten@gmx.de>
2024-04-07 16:25:51 +0200

    fix: direcly use db pkg name in config

--- src/database.mjs
+++ src/database.mjs
@@ -1,28 +1,12 @@
-async function loadDrivers(names) {
-  const drivers = {};
-
-  for (const name of names) {
-    const driver = await import(name);
-    drivers[driver.default.name] = driver.default;
-  }
-
-  return drivers;
-}
-
 export const defaultDatabaseConfig = {
   database: {
-    level: "${statedir}/db"
+    "@konsumation/db-level": "${statedir}/db"
   }
 };
 
 export async function prepareDatabase(config) {
-  const dbTypes = await loadDrivers([
-    "@konsumation/db-level",
-    "@konsumation/db-postgresql"
-  ]);
-
-  for (const [driverName, option] of Object.entries(config.database)) {
-    return { master: await dbTypes[driverName].initialize(option) };
+  for (const [name, option] of Object.entries(config.database)) {
+    return { master: await (await import(name)).default.initialize(option) };
   }
 
   return {};
