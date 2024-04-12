b746b124b13141654f149f4674d5351b960d98b7
Markus Felten <Markus.Felten@gmx.de>
2024-04-12 13:59:06 +0200

    test: cleanup

--- tests/http-auth-ava.mjs
+++ tests/http-auth-ava.mjs
@@ -1,66 +1,11 @@
 import test from "ava";
-import { readFileSync } from "node:fs";
-import { mkdir } from "node:fs/promises";
-import { fileURLToPath } from "node:url";
 import got from "got";
-import { prepareHttpServer } from "../src/http.mjs";
-
-function pn(path) {
-  return fileURLToPath(new URL(path, import.meta.url));
-}
-
-const sd = { notify: () => {}, listeners: () => [] };
+import { startServer, stopServer } from "./helpers/server.mjs";
 
 let port = 3169;
 
-test.before(async t => {
-  await mkdir(pn("../build"), { recursive: true });
-
-  port++;
-
-  const config = {
-    version: "1.2.3",
-    database: {
-      file: pn(`../build/db-${port}`)
-    },
-    auth: {
-      jwt: {
-        public: readFileSync(pn("../config/demo.rsa.pub")),
-        private: readFileSync(pn("../config/demo.rsa")),
-        options: {
-          algorithm: "RS256"
-        }
-      },
-      users: {
-        admin: {
-          password: "start123",
-          entitlements: ["konsum"]
-        }
-      }
-    },
-    http: {
-      port
-    }
-  };
-
-  const { server } = await prepareHttpServer(config, sd);
-
-  let response = await got.post(`http://localhost:${port}/authenticate`, {
-    json: {
-      username: "admin",
-      password: "start123"
-    }
-  });
-
-  t.context.token = JSON.parse(response.body).access_token;
-  t.context.server = server;
-  t.context.port = port;
-});
-
-test.after.always(async t => {
-  t.context.server.close();
-  t.context.server.unref();
-});
+test.beforeEach(t => startServer(t, port++));
+test.afterEach(t => stopServer(t));
 
 test("server can authenticate", async t => {
   const response = await got.post(
